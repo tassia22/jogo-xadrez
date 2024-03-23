9a90ae7c95688891a54d5595d88254e8951373fa
Tássia Pereira <tassiapereira2020@gmail.com>
2024-03-23 20:37:49 -0300

    redirecionando exceptions e clearing screan

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -2,8 +2,10 @@ package Aplicacao;
 import Tabuleiro.Tabuleiro;
 import Xadrez.PartidaXadrez;
 import Xadrez.PecaXadrez;
+import Xadrez.XadrezException;
 import Xadrez.XadrezPosicao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Programa {
@@ -11,20 +13,30 @@ public class Programa {
         Scanner scan = new Scanner(System.in);
 
         PartidaXadrez partidaXadrez = new PartidaXadrez();
-        while (true){
-            //recebe a matriz de peças da minha partida
-            UI.printTabuleiro(partidaXadrez.getPecas());
-            System.out.println();
-            System.out.print("origem: ");
-            XadrezPosicao origem = UI.lerPosicaoXadrez(scan);
+        while (true) {
+            try {
+                UI.clearScreen();
+                //recebe a matriz de peças da minha partida
+                UI.printTabuleiro(partidaXadrez.getPecas());
+                System.out.println();
+                System.out.print("origem: ");
+                XadrezPosicao origem = UI.lerPosicaoXadrez(scan);
 
-            System.out.println();
-            System.out.print("destino: ");
-            XadrezPosicao destino = UI.lerPosicaoXadrez(scan);
+                System.out.println();
+                System.out.print("destino: ");
+                XadrezPosicao destino = UI.lerPosicaoXadrez(scan);
 
-            PecaXadrez capturaPeca = partidaXadrez.executarMovimentoXadrez(origem, destino);
+                PecaXadrez capturaPeca = partidaXadrez.executarMovimentoXadrez(origem, destino);
+            }
+            catch (XadrezException x){
+                System.out.println(e.getMessage());
+                scan.nextLine();
+            }
+            catch (InputMismatchException x){
+                System.out.println(e.getMessage());
+                scan.nextLine();
+            }
         }
-
 
     }
 }
--- src/Aplicacao/UI.java
+++ src/Aplicacao/UI.java
@@ -31,6 +31,10 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
     public  static XadrezPosicao lerPosicaoXadrez(Scanner scan){
         try {
             String s = scan.nextLine();
